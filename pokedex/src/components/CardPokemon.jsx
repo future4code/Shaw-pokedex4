e6f54a51f557fb7735fdd037851850865bf59857
Rodrigo Santos <rodrigo.v.santos89@gmail.com>
2022-05-03 17:42:35 -0300

    03/05 entrega

--- pokedex/src/components/CardPokemon.jsx
+++ pokedex/src/components/CardPokemon.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
 
-const CardFather = styled.div`
-  display: flex;
-  flex-direction: column;
-  border: 1px solid black;
-  width: 40vh;
-  align-items: center;
-`;
 
-export default function CardPokemon() {
+const CardFather = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    width: 40vh;
+    align-items: center;
+    
+    
+`
+
+export default function CardPokemon(props) {
+
+  const [pokemon, setPokemon] = useState({})
+
+  const getAllPokeCard = () => {
+
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${props.nome}`)
+      .then((res) => {
+        setPokemon(res.data)
+
+      })
+      .catch((err) => {
+        alert("[ERRO]")
+      })
+  };
+
+  useEffect(() => {
+
+    getAllPokeCard()
+
+  }, []);
+
+
   return (
     <center>
       <CardFather>
-        <foto>
-          <img src="https://www.imagensanimadas.com/data/media/1446/pokemon-imagem-animada-0100.gif" />
-        </foto>
+        <>
+        {pokemon.sprites && (
+        <img
+          src={pokemon.sprites.front_default}
+          alt={pokemon.name}/>)};
+
+        </>
         <button>REMOVER DA POKEDEX</button>
         <button>VER DETALHES</button>
       </CardFather>
+
     </center>
   );
 }
