7f3aa6d529ea3ae7ff26b86d2873cffa3365b3d3
Pablo Gomes <pablogomes_bm@hotmail.com>
2022-05-05 17:12:48 -0300

    Pokedex funcionando

--- pokedex/src/pages/Pokedex/Pokedex.jsx
+++ pokedex/src/pages/Pokedex/Pokedex.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import axios from "axios";
 import styled from "styled-components";
+import CardPokemon from "../../components/CardPokemon";
+import { goToHomePage } from "../../routes/Coordinator";
+import { useNavigate } from "react-router-dom";
+import CardPokedex from "../../components/CardPokedex";
 
-export default function Pokedex() {
+
+
+export default function Pokedex(props) {
+  const navigate = useNavigate()
+
+
+const mostrarNapokedex = props.pokemonsInPokedex.map((pokemon)=>{
+  console.log(pokemon)
+  return <CardPokedex removeFromPokedex={props.removeFromPokedex} pokemon={pokemon} />
+
+})
+
+
   return (
     <>
-      <h1>Pokedex </h1>
+    <h1>Pokedex</h1>
+  
+
+
+
+
+      {mostrarNapokedex}
+      <button onClick={()=>goToHomePage(navigate)}>Home</button>
     </>
   );
 }
